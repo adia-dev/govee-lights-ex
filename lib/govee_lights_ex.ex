d1ac3bc35ee5f1386187b634f25b7bad30b11ef8
Abdoulaye Dia <abdoudu78130@gmail.com>
2025-11-28 00:02:03 +0100

    ✨ Request to get the devices of an account by GOVEE_API_KEY

--- lib/govee_lights_ex.ex
+++ lib/govee_lights_ex.ex
@@ -3,6 +3,10 @@ defmodule GoveeLights do
   Documentation for `GoveeLights`.
   """
 
+  @govee_api_key "GOVEE_API_KEY"
+  @govee_base_url "https://developer-api.govee.com/v1"
+  @govee_endpoints [devices: "/devices", state: "/state"]
+
   @doc """
   Hello world.
 
@@ -14,5 +18,34 @@ defmodule GoveeLights do
   """
   def hello do
     :world
+  end
+
+  def devices do
+    api_key = api_key!()
+
+    request =
+      Req.new(
+        method: :get,
+        url: "#{@govee_base_url}#{@govee_endpoints[:devices]}",
+        headers: %{govee_api_key: api_key}
+      )
+
+    case Req.get(request) do
+      {:ok, response} ->
+        response.body["data"]["devices"]
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  defp api_key!() do
+    case System.get_env(@govee_api_key) do
+      nil ->
+        exit("#{@govee_api_key} must be set.")
+
+      api_key ->
+        api_key
+    end
   end
 end
